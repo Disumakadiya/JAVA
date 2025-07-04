9e4c01db56ed08bc389e7e8d3c5a8394da38d918
Disumakadiya <disumakadiya@gmail.com>
2025-07-04 22:25:39 +0530

    add

--- JavaBasics/practice_programs/patterns/Pattern5.java
+++ JavaBasics/practice_programs/patterns/Pattern5.java
@@ -2,7 +2,8 @@ package patterns;
 import java.util.Scanner;
 public class Pattern5
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         try (java.util.Scanner input = new Scanner(System.in))
         {
             System.out.print("Enter the number of rows: ");
